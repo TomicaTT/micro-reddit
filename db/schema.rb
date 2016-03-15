75d4aad5ffe7d6b88752c3a46940c780fd79486a
TomicaTT <stefan1612@its.edu.rs>
2016-03-15 16:00:36 +0100

    Rake database migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315144006) do
+ActiveRecord::Schema.define(version: 20160315145844) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
